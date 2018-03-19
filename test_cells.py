5a3a7a3ea5741d003f110ff716bd61738456eff4
Jacob Glines <jglines14@yahoo.com>
2018-03-18 23:49:40 -0500

    Updated unittests

--- test_cells.py
+++ test_cells.py
@@ -1,16 +1,34 @@
 import unittest
 from cells import Cell
 
-class CellTestCase(unittest.TestCase):
-    def test_constructor(self):
-        cell = Cell(True)
-        self.assertTrue(cell.living)
 
-    def test_string(self):
-        cell = Cell(True)
+class CellTestCase(unittest.TestCase):
+
+    def test_create_cell(self):
+        row = 1
+        column = 2
+        cell = Cell(row, column)
+        self.assertEqual(cell.alive, False)
+        self.assertEqual(cell.row, row)
+        self.assertEqual(cell.column, column)
+
+    def test_dead_cells(self):
+        cell = Cell(0, 0)
+        cell.die()
+        self.assertFalse(cell.alive)
+
+    def test_live_cells(self):
+        cell = Cell(0, 0)
+        cell.live()
+        self.assertTrue(cell.alive)
+
+    def test_str(self):
+        cell = Cell(0, 0)
+        cell.live()
         self.assertEqual('0', str(cell))
-        cell.living = False
+        cell.die()
         self.assertEqual('-', str(cell))
+
 
 if __name__ == '__main__':
     unittest.main()
--- test_world.py
+++ test_world.py
@@ -9,6 +9,11 @@ class WorldTestCase(unittest.TestCase):
         world = World(5, 10)
         self.assertEqual(5, world.rows)
         self.assertEqual(10, world.columns)
+        for row in range(world.rows):
+            for column in range(world.columns):
+                self.assertFalse(world.cells[row][column].alive)
+                self.assertEqual(world.cells[row][column].row, row)
+                self.assertEqual(world.cells[row][column].column, column)
 
     def test_row(self):
         world = World(5, 10)
@@ -18,12 +23,32 @@ class WorldTestCase(unittest.TestCase):
         world = World(10, 5)
         self.assertEqual(5, world.columns)
 
-    def test_string(self):
+    def test_str(self):
         world = World(0, 0)
-        alive = Cell(True)
-        dead = Cell(False)
+        alive = Cell(0, 0)
+        alive.live()
+        dead = Cell(0, 1)
         world.cells = [[alive, dead], [dead, alive]]
         self.assertEqual(str(world), f'| 0 | - | \n| - | 0 | \n')
+
+    def test_populate(self):
+        rows = 10
+        columns = 10
+        w = World(rows, columns)
+        w.populate_cells(0)
+        self.assertEqual(w.count_living(), 0)
+        w.populate_cells(50)
+        self.assertEqual(w.count_living(), 50)
+        w.populate_cells(100)
+        self.assertEqual(w.count_living(), 100)
+
+
+    def test_count_living(self):
+        w = World(10, 10)
+        living = 30
+        w.populate_cells(living)
+        self.assertEqual(living, w.count_living())
+
 
 
 if __name__ == '__main__':
