f6e2aac84693b6f6187496e03712471d7ae5eaa0
Jacob Glines <jglines14@yahoo.com>
2018-03-18 23:48:33 -0500

    Added live and die function

--- cells.py
+++ cells.py
@@ -1,6 +1,17 @@
 class Cell:
-    def __init__(self, living):
-        self.living = living
+    def __init__(self, row, column):
+        self.alive = False
+        self.row = row
+        self. column = column
+        self.location = None
+
+    def live(self):
+        self.alive = True
+        return self
+
+    def die(self):
+        self.alive = False
+        return self
 
     def __repr__(self):
-        return '0' if self.living else '-'
+        return '0' if self.alive else '-'
